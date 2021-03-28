e6333209be530ed0436475c41b4678e14471d124
Juan David González <69774985+TOYCRESJDGM@users.noreply.github.com>
2021-03-28 14:45:28 -0500

    Added Login service  (#6)
    * Update gitignore
    
    * Added login service
    
    * Update CHANGELOG.md

--- application/controllers/UserController.js
+++ application/controllers/UserController.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const {User} = require('../Config/db');
+const tokenServices = require('../services/token');
 
 
 exports.add = async(req,res,next)=>{
@@ -20,6 +21,45 @@ exports.add = async(req,res,next)=>{
     }catch(error){
         res.status(500).send({
             message: 'Error en el servidor.'
+        })
+        next(error);
+    }
+};
+
+exports.login = async(req,res,next)=>{
+    try{
+        const Usuario = await User.findOne({where: {email: req.body.email}});
+        if(Usuario){
+            const passwordIsValid = bcrypt.compareSync(req.body.password, Usuario.password);
+            if(passwordIsValid){
+                const token = await tokenServices.encode(Usuario);
+                res.status(200).send({
+                    message: 'Bienvenido',
+                    token : token,
+                    user :
+                    {   
+                        id : Usuario.user_id,
+                        name: Usuario.user_name,
+                        email: Usuario.email
+                    } 
+                    
+                })
+
+            }else {           
+                //error en la autenticación
+                res.status(401).json({
+                    error: 'Error en el ususario o contraseña'
+                })
+            }
+        }else{
+            //error en la autenticación
+            res.status(404).json({
+                error: 'Error en el ususario o contraseña'
+            })
+        }
+    }catch(error){
+        res.status(500).send({
+            message: 'Error ->' + error
         })
         next(error);
     }
