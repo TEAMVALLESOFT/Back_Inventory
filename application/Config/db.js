467f4097eb78cddc3ce4fdd8c289b0b7a7b8a650
Juan David González <69774985+TOYCRESJDGM@users.noreply.github.com>
2021-03-27 20:47:45 -0500

    Update db.js

--- application/Config/db.js
+++ application/Config/db.js
@@ -2,8 +2,8 @@ const Sequelize = require('sequelize');
 
 const UserModel = require('../models/user');
 
-const sequelize = new Sequelize('SicDMSiMjC', 'SicDMSiMjC', 'QTnWiJvXZ7', {
-    host: 'remotemysql.com',
+const sequelize = new Sequelize('Database_name', 'user_name', 'password', {
+    host: 'host_name',
     dialect: 'mysql'
 });
 
