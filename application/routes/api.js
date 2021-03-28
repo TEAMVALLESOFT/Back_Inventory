357fd42c2f8b213f108d19d07c8c8645dffbb444
TOYCRESJDGM <juandavid00412@gmail.com>
2021-03-28 17:37:34 -0500

    Added Warehouse create service

--- application/routes/api.js
+++ application/routes/api.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const UsersRouter = require('./api/users');
+const WarehouseRouter = require('./api/warehouse');
 
 router.use('/user', UsersRouter);
+router.use('/warehouse', WarehouseRouter);
 
 module.exports = router;
