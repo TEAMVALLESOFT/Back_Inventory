750b5c4ac7b22379cd27455146e3a490e4a1adc5
Zeta-Z <suescumperea@gmail.com>
2021-03-29 18:49:06 -0500

    The "List all types of articles" service was created

--- application/routes/api/type.js
+++ application/routes/api/type.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const TypeController = require('../../controllers/TypeController');
 
 router.post('/create', TypeController.add);
+router.get('/list',TypeController.list);
 
 
 module.exports = router;
