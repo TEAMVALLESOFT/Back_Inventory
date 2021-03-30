7e9b34f15b6be69b8ad4a514d8efde33b67b90c8
Juan David González <69774985+TOYCRESJDGM@users.noreply.github.com>
2021-03-30 18:53:19 -0500

    Update server.js

--- application/server.js
+++ application/server.js
@@ -11,7 +11,7 @@ const app = express();
     app.use((req,res, next)=> {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
-        res.header('Access-Control-Allow-Methods: GET, POST, DELETE');
+        res.header('Access-Control-Allow-Methods: GET, POST, DELETE, PUT');
         next();
       })
 
