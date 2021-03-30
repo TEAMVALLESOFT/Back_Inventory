d426d468e66a27107c57bc503a11d173e6475131
TOYCRESJDGM <juandavid00412@gmail.com>
2021-03-30 15:48:53 -0500

    fix serverj.js

--- application/server.js
+++ application/server.js
@@ -5,6 +5,13 @@ require('./Config/db');
 const app = express();
     app.use(cors());
 
+    app.use((req,res, next)=> {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+        res.header('Access-Control-Allow-Methods: GET, POST, DELETE');
+        next();
+      })
+
 
 // settings
 app.set('port',process.env.PORT || 3000);
