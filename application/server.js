7f7ff4d062a97b20d23d2dc4c73094c38751442a
Juan David González <69774985+TOYCRESJDGM@users.noreply.github.com>
2021-03-31 15:02:36 -0500

    bugfix server file (#13)
    * Install cors
    
    * fix serverj.js
    
    * Update server.js
    
    * Update server.js

--- application/server.js
+++ application/server.js
@@ -1,10 +1,19 @@
 const express = require ('express');
 const apiRouter = require('./routes/api');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('./Config/db');
+
 const app = express();
     app.use(cors());
 
+
+    app.use((req,res, next)=> {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+        res.header('Access-Control-Allow-Methods: GET, POST, DELETE, PUT');
+        next();
+      })
 
 // settings
 app.set('port',process.env.PORT || 3000);
