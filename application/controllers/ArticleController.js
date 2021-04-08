2ca571bfbeb5805cc0482d2e94027edd1b1bb864
TOYCRESJDGM <juandavid00412@gmail.com>
2021-04-08 15:50:41 -0500

    Update Controller

--- application/controllers/ArticleController.js
+++ application/controllers/ArticleController.js
@@ -21,37 +21,56 @@ exports.create = async (req, res, next) => {
             }
             else {
                 const array = req.body.secondary_article_list;
-                if (array) {
-                    console.log(array.length);
-                    var count = await db.article.count({}) + 1;
-                    var article_label = parent.classif.substring(0, 3) + req.body.branch.substring(0, 3) + '_' + count;
-                    const registro = await db.article.create({
-                        label: article_label,
-                        available_state: req.body.available_state,
-                        physical_state: req.body.physical_state,
-                        branch: req.body.branch,
-                        warehouse_fk: req.body.warehouse_fk,
-                        article_type_fk: req.body.article_type_fk
-                    });
-                    const ObjectParent = await db.article.findOne({where:{id: registro.id}});
-                    console.log(ObjectParent);
-                    if(ObjectParent){
-                        for(var i=0; i<array.length; i++){
-                            const type = await db.article_type.findOne({ where: { id: array[i].article_type_fk } });
-                            var count = await db.article.count({}) + 1;
-                            var articlelabel = type.classif.substring(0, 3) + array[i].branch.substring(0, 3) + '_' + count;
-                            const registro = await db.article.create({
-                                label: articlelabel,
-                                available_state: array[i].available_state,
-                                physical_state: array[i].physical_state,
-                                branch: array[i].branch,
-                                warehouse_fk: array[i].warehouse_fk,
-                                article_type_fk: array[i].article_type_fk,
-                                article_fk: ObjectParent.id
-                            });
+                if (array != null) {
+                    let ican = 0;
+                    for (var j = 0; j < array.length; j++) {
+                        const type = await db.article_type.findOne({ where: { id: array[j].article_type_fk } });
+                        if (type.is_parent === 0) {
+                            ican += 1;
+                        }
+                        else {
+                            ican = ican;
+                        }
+                    }
+                    if (ican === array.length) {
+                        var count = await db.article.count({}) + 1;
+                        var article_label = parent.classif.substring(0, 3) + req.body.branch.substring(0, 3) + '_' + count;
+                        const registro = await db.article.create({
+                            label: article_label,
+                            available_state: req.body.available_state,
+                            physical_state: req.body.physical_state,
+                            branch: req.body.branch,
+                            warehouse_fk: req.body.warehouse_fk,
+                            article_type_fk: req.body.article_type_fk
+                        });
+                        const ObjectParent = await db.article.findOne({ where: { id: registro.id } });
+                        if (ObjectParent) {
+                            for (var i = 0; i < array.length; i++) {
+                                const type = await db.article_type.findOne({ where: { id: array[i].article_type_fk } });
+                                if (type.is_parent === 0) {
+                                    var count = await db.article.count({}) + 1;
+                                    var articlelabel = type.classif.substring(0, 3) + array[i].branch.substring(0, 3) + '_' + count;
+                                    const registro = await db.article.create({
+                                        label: articlelabel,
+                                        available_state: array[i].available_state,
+                                        physical_state: array[i].physical_state,
+                                        branch: array[i].branch,
+                                        warehouse_fk: array[i].warehouse_fk,
+                                        article_type_fk: type.id,
+                                        article_fk: ObjectParent.id
+                                    });
+                                }
+                            }
+
                         }
                         res.status(200).send({
                             message: 'El artículo fue creado con éxito.'
+                        });
+                    }
+
+                    else {
+                        res.status(404).send({
+                            message: 'No es posible realizar la creación y asociación del articulo.'
                         });
                     }
                 }
@@ -67,7 +86,6 @@ exports.create = async (req, res, next) => {
                 message: 'No se encontro el tipo de articulo.'
             });
         }
-
     } catch (error) {
         res.status(500).send({
             message: 'Error'
