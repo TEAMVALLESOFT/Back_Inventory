59a183b338ff6e36dedbd6a4d95bce3f80fe625b
Luis Suescum <63677060+Zeta-Z@users.noreply.github.com>
2021-04-08 21:32:04 -0500

    Update 20210409010152-create-reservation.js

--- application/migrations/20210409010152-create-reservation.js
+++ application/migrations/20210409010152-create-reservation.js
@@ -9,9 +9,11 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       borrowing_fk: {
+        allowNull: false,
         type: Sequelize.INTEGER
       },
       articles_fk: {
+        allowNull: false,
         type: Sequelize.INTEGER
       },
       createdAt: {
