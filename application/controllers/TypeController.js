16b38db7c999972c81bf9bd07648e56f8c32c821
TOYCRESJDGM <juandavid00412@gmail.com>
2021-04-08 19:50:12 -0500

    Update typecontroller list

--- application/controllers/TypeController.js
+++ application/controllers/TypeController.js
@@ -1,24 +1,24 @@
-const db = require ('../models');
+const db = require('../models');
 
 
-exports.add = async(req,res,next)=>{
-    try{  
-        const typee = await db.article_type.findOne({where: {article_type_name: req.body.article_type_name}});
+exports.add = async (req, res, next) => {
+    try {
+        const typee = await db.article_type.findOne({ where: { article_type_name: req.body.article_type_name } });
 
-        if(typee){
+        if (typee) {
             res.status(409).send({
                 message: 'El tipo de artículo deseado ya existe.'
             })
         }
-        else{
+        else {
             const Object = await db.article_type.create(req.body);
-            if(Object){
+            if (Object) {
                 res.status(200).send({
                     message: 'Tipo de artículo creado exitosamente.'
-                 });
+                });
             }
         }
-    }catch(error){
+    } catch (error) {
         res.status(500).send({
             message: 'Error en el servidor.'
         })
@@ -26,42 +26,40 @@ exports.add = async(req,res,next)=>{
     }
 };
 
-exports.list  = async(req,res,next)=>{
+exports.list = async (req, res, next) => {
 
-    try{
-        const ListA =await db.article_type.findAll()
-        if(ListA){
-
-            res.status(200).json(ListA);
-        }else{
-
-            res.status(204).send({
-                message:'No hay registros en el sistema'
+    try {
+        const { classif } = req.query;
+        if (classif) {
+            const type = await db.article_type.findAll({
+                where: { classif: classif }
             });
+            if (type) {
+
+                res.status(200).json(type);
+            } else {
+
+                res.status(204).send({
+                    message: 'No hay registros en el sistema'
+                });
+            }
         }
-        //return res.json(ListA)
-    }catch(err){
+        else {
+            const type = await db.article_type.findAll()
+            if (type) {
+
+                res.status(200).json(type);
+            } else {
+
+                res.status(204).send({
+                    message: 'No hay registros en el sistema'
+                });
+            }
+        }
+    } catch (err) {
         console.log(err)
-        return res.status(500).json({error: 'Something went went wrong'})
+        return res.status(500).json({ error: '¡Error en el servidor!.' });
 
-    } 
+    }
 };
 
-exports.ListbyClassif  = async(req,res,next)=>{
-
-    try{
-        const {classif} = req.params;
-        const type = await db.article_type.findAll({
-            where: {classif: classif}
-        });
-        if(type){
-            res.status(200).json(type);
-        }else{
-            res.status(404).send({
-                message:'No se encontraron registros.'
-            });
-        }
-    }catch(err){
-        return res.status(500).json({error: '¡Error en el servidor!.'});
-    } 
-};
--- application/routes/api/type.js
+++ application/routes/api/type.js
@@ -3,7 +3,6 @@ const TypeController = require('../../controllers/TypeController');
 
 router.post('/create', TypeController.add);
 router.get('/list',TypeController.list);
-router.get('/list/:classif', TypeController.ListbyClassif);
 
 
 module.exports = router;
