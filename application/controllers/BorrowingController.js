a0b3e39182f8bb7d55e07b5bed591dd324d13798
Juan David González <69774985+TOYCRESJDGM@users.noreply.github.com>
2021-04-14 15:07:24 -0500

    Update BorrowingController.js

--- application/controllers/BorrowingController.js
+++ application/controllers/BorrowingController.js
@@ -22,7 +22,7 @@ exports.create = async (req, res, next) => {
                         auth_state: "Pendiente",
                         pick_up_date: req.body.pick_up_date,
                         return_date: req.body.return_date,
-                        has_returning: req.body.has_returning,
+                        has_returning: 0,
                     });
                     const borrowing = await db.borrowing.findOne({ where: { id: registro.id } });
                     if (borrowing != null) {
